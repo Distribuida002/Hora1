67afc7abadb9b2304ecf4d04008abc14c8c0d0a0
Distribuida002 <Distribuida002@gmail.com>
2025-05-14 15:43:52 -0500

    Update hora.py

--- hora.py
+++ hora.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
 @app.route("/")
 def hora_actual():
-    ahora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h2>Hora actual del servidor:</h2><p>{ahora}</p>"
+    zona_ecuador = pytz.timezone("America/Guayaquil")
+    ahora = datetime.now(zona_ecuador).strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h2>Hora actual en Ecuador:</h2><p>{ahora}</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
